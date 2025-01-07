48973c076662067e4af207e9cc8bb613950be52e
toufikunsure-4529 <toufiksk.ab3@gmail.com>
2025-01-08 04:28:09 +0530

    Fix Producsts json file Api call issue

--- src/components/common/Explore.jsx
+++ src/components/common/Explore.jsx
@@ -10,21 +10,21 @@ function Explore({ ViewAllBtnVisible = true }) {
     {
       id: 1,
       itemName: "Nex Dryft",
-      slug: "",
+      slug: "/ceiling-fans",
       images: "/images/H_dryft.png",
       hoverImageUrl: "/images/Nex_Dryft.jpg",
     },
     {
       id: 2,
       itemName: "Nex Giyde",
-      slug: "",
+      slug: "/ceiling-fans",
       images: "/images/H_glyde.png",
       hoverImageUrl: "/images/Nex_Glyde.jpg",
     },
     {
       id: 3,
       itemName: "Nex TPW",
-      slug: "",
+      slug: "/ceiling-fans",
       images: "/images/explore-section-TPW.png",
       hoverImageUrl: "/images/explore-section-TPW-hover.jpg",
     },
@@ -63,26 +63,28 @@ function Explore({ ViewAllBtnVisible = true }) {
             // Mobile View: Slick Slider
             <Slider {...sliderSettings} className="w-full">
               {exploreCategories.map((item) => (
-                <div key={item.id} className="px-4">
-                  <div className="group relative p-8 bg-[#131313] w-full rounded-xl shadow flex flex-col justify-center items-center gap-8 overflow-hidden">
-                    <p className="text-lg font-semibold z-10">
-                      {item.itemName}
-                    </p>
-                    <img
-                      src={item.images}
-                      alt={item.itemName}
-                      className="z-10"
-                    />
-                    <button className="uppercase border border-cyan-300 rounded-full text-cyan-300 h-11 w-auto px-9 py-2 z-10">
-                      View This
-                    </button>
-                    <img
-                      src={item.hoverImageUrl}
-                      alt={`${item.itemName} hover`}
-                      className="absolute top-0 left-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out z-[99]"
-                    />
+                <Link to={`${item.slug}?id=${item.id}`} key={item.id}>
+                  <div key={item.id} className="px-4">
+                    <div className="group relative p-8 bg-[#131313] w-full rounded-xl shadow flex flex-col justify-center items-center gap-8 overflow-hidden">
+                      <p className="text-lg font-semibold z-10">
+                        {item.itemName}
+                      </p>
+                      <img
+                        src={item.images}
+                        alt={item.itemName}
+                        className="z-10"
+                      />
+                      <button className="uppercase border border-cyan-300 rounded-full text-cyan-300 h-11 w-auto px-9 py-2 z-10">
+                        View This
+                      </button>
+                      <img
+                        src={item.hoverImageUrl}
+                        alt={`${item.itemName} hover`}
+                        className="absolute top-0 left-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out z-[99]"
+                      />
+                    </div>
                   </div>
-                </div>
+                </Link>
               ))}
             </Slider>
           ) : (
@@ -92,7 +94,7 @@ function Explore({ ViewAllBtnVisible = true }) {
               data-aos="fade-up"
             >
               {exploreCategories.map((item) => (
-                <Link to={item.slug} key={item.id}>
+                <Link to={`${item.slug}?id=${item.id}`} key={item.id}>
                   <div className="group relative p-8 bg-[#131313] w-72 rounded-xl shadow flex flex-col justify-center items-center gap-8 overflow-hidden">
                     <p className="text-lg font-semibold z-10">
                       {item.itemName}
